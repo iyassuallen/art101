be4ca2f59707bd20c68898708a9b8eb01cfc23f5
Iyassu Allen <iyassu.allen@gmail.com>
2022-10-12 21:16:28 -0700

    lab4
    images and html and stuff

---
+++ lab4/js/lab.js
@@ -0,0 +1,33 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+
+//Pseudocode for cooking a chicken breast:
+//Make sure you have all the tools and ingredients (Chicken breast, seasonings of your choice (ex: salt and pepper), pan, oven, knife (optional), olive oil (or some oil of your choice), meat mallet (preferred), and a cutting board of some sort.
+  //Ensure the chicken breast is fully thawed (the best method is to leave it in the fridge for at least 24 hours).
+  //Evenly flatten chicken breast with a meat mallet on your cutting board.
+  //Apply a generous amount of salt and a couple of pinches of pepper to both sides.
+  //Allow chicken to sit at room temperature for 20 minutes.
+  //Preheat the pan for 2 minutes over medium heat.
+  //Apply 2 tbs of olive oil to the pan, making sure to cover the pan fully.
+  //Let the oil heat up for 30 seconds.
+  //Apply chicken breasts to the pan, and let it sit for 4 minutes.
+  //Flip the chicken breast, and let it sit for 3 minutes.
+//Let the chicken breast sit for 5 minutes before cutting into it, and enjoy.
+
+
+//Pseudocode for a double-jump in a video game.
+//Already have a jumping mechanic setup.
+  //Create a value that counts the number of available jumps for your player, set it to 2.
+  //Create a condition that when the player jumps, 1 unit of the available jumps is depleted and they cannot jump if that value is at 0.
+//Create a boolean that checks if your player is grounded (if the player's model is colliding with the ground).
+  //Reset the value of the available jumps to 2 if the grounded boolean is true.
