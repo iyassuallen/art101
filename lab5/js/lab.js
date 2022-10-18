bf6ba9938d3c19e73f1d005052ab8f50bca6b5c9
Iyassu Allen <iyassu.allen@gmail.com>
2022-10-17 20:58:58 -0700

    lab5, js, index.html

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Iyassu Allen
+ * Created:   10.17.2022
+ *
+ *
+ **/
+
+//variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+//calculate age
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
