3e371217fa7a0bce66696179a4c9f37ef1c77de5
Iyassu Allen <iyassu.allen@gmail.com>
2022-11-14 19:47:48 -0800

    FizzBuzz lab13

---
+++ lab13/js/lab.js
@@ -0,0 +1,34 @@
+/**
+ * Author: Iyassu Allen
+ * Created:   11.14.2022
+ *
+ **/
+function FizzBuzz(){
+  for(i=0; i<=200; i++){
+    str = "";
+    if(i % 3 == 0){
+      str = str + "Fizz";
+      console.log(str);
+      $("#output").append("<p>" + str + "</p>");
+    }
+    if(i % 5 == 0){
+      str = str + "Buzz";
+      console.log(str);
+      $("#output").append("<p>" + str + "</p>");
+    }
+    if(i % 7 == 0){
+      str = str + "Fizz";
+      console.log(str);
+      $("#output").append("<p>" + str + "</p>");
+    }
+    if(str != ""){
+      console.log(str);
+      $("#output").append("<p>" + str + "</p>");
+    }
+    else{
+      console.log(i);
+      $("#output").append("<p>" + i + "</p>");
+    }
+  }
+}
+FizzBuzz()
